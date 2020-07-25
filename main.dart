e1f15e5c698490e4a82a513b2b86e387403813d7
shikhagupta63 <66768096+shikhagupta63@users.noreply.github.com>
2020-07-25 22:29:22 +0530

    upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+//import 'zzui/basic.dart';
+import 'zzui/basic2.dart';
+
+main() {
+  runApp(Myprofile());
+}
+
+class Myprofile extends StatelessWidget {
+  build(BuildContext c1) {
+    return myapp2();
+  }
+}
